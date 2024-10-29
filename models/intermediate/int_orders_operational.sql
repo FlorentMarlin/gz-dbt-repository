d5c6c1cae237d95368b72d7a01a18aafec64f190
g3iumy42j@mozmail.com <g3iumy42j@mozmail.com>
2024-10-29 15:10:05 +0000

    Added operational margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,5 @@
+SELECT *
+, margin + shipping_fee - logcost - ship_cost AS operational_margin
+FROM {{ ref('int_orders_margin') }} as table_margin
+LEFT JOIN {{ ref('stg_raw__ship')}} as table_ship
+USING(orders_id)
