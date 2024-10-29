5dc44d49ad840b84f2f502b9ac14dd9392dd9a4a
g3iumy42j@mozmail.com <g3iumy42j@mozmail.com>
2024-10-29 14:37:30 +0000

    Added derivation of purchase_cost

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,5 @@
 SELECT *
+, sales.quantity * product.purchase_price AS purchase_cost
 FROM {{ ref('stg_raw__sales') }} AS sales
 LEFT JOIN {{ ref('stg_raw__product') }} AS product
 USING(products_id)
