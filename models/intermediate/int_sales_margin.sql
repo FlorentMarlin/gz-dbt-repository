5f846276eaa647592facfd401a77d197596e8c41
g3iumy42j@mozmail.com <g3iumy42j@mozmail.com>
2024-10-30 14:20:18 +0000

    Added first macro WIP. Added use of dbt-utils.

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,6 @@
 SELECT *
 , sales.quantity * product.purchase_price AS purchase_cost
+, {{ margin_percent('product.purchase_price', 'sales.quantity') }} AS margin_percent
 FROM {{ ref('stg_raw__sales') }} AS sales
 LEFT JOIN {{ ref('stg_raw__product') }} AS product
 USING(products_id)
